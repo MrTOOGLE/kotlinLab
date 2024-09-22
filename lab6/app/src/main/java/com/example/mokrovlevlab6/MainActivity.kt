90594b81098d1a2b81ce349e170396a30d3705bf
MrTOOGLE <Leva.mokrov@gmail.com>
2024-09-22 13:43:33 +0700

    boba

--- lab6/app/src/main/java/com/example/mokrovlevlab6/MainActivity.kt
+++ lab6/app/src/main/java/com/example/mokrovlevlab6/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.mokrovlevlab6
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.doAfterTextChanged
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var a: EditText
@@ -27,5 +30,37 @@ class MainActivity : AppCompatActivity() {
         b = findViewById(R.id.editTextText2)
         c = findViewById(R.id.editTextText3)
         answer = findViewById(R.id.textView8)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //TODO: как-то улучшить
+        a.doAfterTextChanged {
+            b.doAfterTextChanged {
+                c.doAfterTextChanged {
+                    calc()
+                }
+            }
+        }
+    }
+
+    private fun calc() {
+        val aa = a.text.toString().toDoubleOrNull()
+        val bb = b.text.toString().toDoubleOrNull()
+        val cc = c.text.toString().toDoubleOrNull()
+
+        if (aa == null || bb == null || cc == null) {
+            answer.setText("Неверный ввод")
+            //Toast.makeText(this, "╚(•⌂•)╝", Toast.LENGTH_SHORT).show()
+        } else {
+            val D: Double = (bb * bb) - (4 * aa * cc)
+            if (D < 0) {
+                answer.setText("Корней нет")
+            } else if (D == 0.0) {
+                answer.setText("Найден один корень:\n${-bb / (2 * aa)}")
+            } else {
+                answer.setText("Найдено два корня:\n${(-bb + sqrt(D)) / (2 * aa)}\n${(-bb - sqrt(D)) / (2 * aa)}")
+            }
+        }
     }
 }
